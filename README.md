cf6327d3eceeab66a27265e0c85dfaea32b4d85d
Novak <novaldix@gmail.com>
2023-05-29 13:06:59 +0300

    17_4_2 added solution

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 * Функция принимает корректные типы данных, тип возвращаемого значения --  void
 * Функция не использует библиотек кроме <iostream>
 * По переданному указателю лежат переменные в обратном порядке
+
+### <font color = "#tomato">Интересные ссылки</font>
+
+[Массивы в параметрах функции](https://metanit.com/cpp/tutorial/4.7.php)
+
+[Function with a size_t Template Parameter](https://stackoverflow.com/questions/34968441/function-with-a-size-t-template-parameter)
--- main.cpp
+++ main.cpp
@@ -3,6 +3,42 @@
 using std::cout;
 using std::endl;
 
-int main() {
+// Вариант 1: передаем массив как указатель на начало массива
+void flipArrA (int *arr, size_t size) {
+    for (size_t i{}; i < (size / 2); ++i) {
+        int temp = *(arr + i);
+        *(arr + i) = *(arr + size - i - 1);
+        *(arr + size - i - 1) = temp;
+    }
+}
 
+// Вариант 2: передаем массив как ссылку
+template<size_t T>
+void flipArrB (int (&arr)[T]) {
+    for (size_t i{}; i < T / 2; ++i) {
+        int temp = *(arr + i);
+        *(arr + i) = *(arr + T - i - 1);
+        *(arr + T - i - 1) = temp;
+    }
+}
+
+template<size_t T>
+void printArr (const int (&arr)[T]) {
+    for (int i = 0; i < T; ++i) {
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    cout << "Begin:\n";
+    printArr(arr);
+    flipArrA(arr, sizeof(arr) / sizeof(*arr));
+    cout << "Flip as pointer:\n";
+    printArr(arr);
+    cout << "Flip as reference:\n";
+    flipArrB(arr);
+    printArr(arr);
 }
